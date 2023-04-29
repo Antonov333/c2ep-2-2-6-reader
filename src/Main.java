f9291f5bf8ac6f0ad72fe5354204c7d0e79e52b6
Antonov333 <antonovsergei333@yahoo.com>
2023-04-29 17:03:59 +0300

    Generally, completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!\nCourse2-extra-problems-2-2-6-reader");
+
+        Random r = new Random();
+
+        Reader john = new Reader("John", "Java Development", "2005-01-15", 1235550000L + (long) r.nextInt(1000));
+        System.out.println(john);
+
+        john.takeBook("Effective Java");
+        john.takeBook("Spring for beginners");
+        john.takeBook("Harry Potter and OOP");
+        System.out.println(john);
+
+        final String[] b = new String[john.getBookNames().length];
+
+        for (int i = 0; i < john.getBookNames().length; i++) {
+            b[i] = john.getBookNames()[i];
+        }
+
+        System.out.println(Arrays.toString(b));
+
+        john.returnBooks(b);
+
+        System.out.println(john);
+
+        john.returnBooks(b);
+
+        Reader[] readers = {john, new Reader("Mary", "Graphic Design", "2004-07-22", 1235550000L + (long) r.nextInt(1000)),
+                new Reader("Lisa", "Management", "2005-09-03", 1235550000L + (long) r.nextInt(1000))};
+
+        String[] books = {"Detective", "Romantic", "Funny stories"};
+
+        for (int i = 0; i < readers.length; i++) {
+            readers[i].takeBook(b[r.nextInt(0, 3)]);
+            readers[i].takeBook(books[r.nextInt(0, 3)]);
+        }
+
+
     }
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,2 +1,131 @@
+import java.util.Arrays;
+
 public class Reader {
+
+    private static int readerCount;
+
+    /**
+     * 1. ФИО
+     * 2. номер читательского билета
+     * 3. факультет
+     * 4. дата рождения
+     * 5. телефон
+     * 6. Методы takeBook(), returnBook().
+     */
+
+
+    private String name;
+    private int cardNo;
+    private String faculty;
+    private String dateOfBirth;
+    private long phone;
+    private String[] bookNames;
+
+    public Reader(String name, String faculty, String dateOfBirth, long phone) {
+        readerCount++;
+        this.name = name;
+        this.faculty = faculty;
+        this.dateOfBirth = dateOfBirth;
+        this.phone = phone;
+        this.cardNo = readerCount;
+    }
+
+    public void takeBook(String newBookName) {
+        int bookCount = 0;
+        if (this.bookNames != null) {
+            bookCount = this.bookNames.length;
+        }
+        bookCount++;
+        String[] bookNames = new String[bookCount];
+        bookNames[bookCount - 1] = newBookName;
+        for (int i = 0; i <= bookCount - 2; i++) {
+            bookNames[i] = this.bookNames[i];
+        }
+        this.bookNames = bookNames;
+        System.out.println(name + " rented book " + bookNames[bookNames.length - 1]);
+    }
+
+    ;
+
+    public void returnBook(String book) {
+        if (bookNames == null) {
+            System.out.println("Book " + book + " is not rented by " + name);
+            return;
+        }
+        boolean returned = false;
+        int returnedBookPointer = -1;
+        for (int i = 0; i <= bookNames.length - 1; i++) {
+            if (bookNames[i] == book) {
+                bookNames[i] = "";
+                returnedBookPointer = i;
+                returned = true;
+                break;
+            }
+        }
+        if (returned) {
+            if (bookNames.length <= 1) {
+                bookNames = null;
+            } else {
+                String[] newBookNames = new String[bookNames.length - 1];
+                for (int i = 0; i < returnedBookPointer; i++) {
+                    newBookNames[i] = bookNames[i];
+                }
+                for (int i = returnedBookPointer + 1; i <= bookNames.length - 1; i++) {
+                    newBookNames[i - 1] = bookNames[i];
+                }
+                bookNames = newBookNames;
+            }
+            System.out.println(name + " returned book " + book);
+        }
+        if (!returned) {
+            System.out.println("Book " + book + " is not rented by " + name);
+        }
+
+    }
+
+    public void returnBooks(String[] bookNames) {
+        if (this.bookNames == null) {
+            System.out.println(name + " has not rented any book yet");
+            return;
+        }
+        for (int i = 0; i < bookNames.length; i++) {
+            returnBook(bookNames[i]);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCardNo() {
+        return cardNo;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public String[] getBookNames() {
+        return bookNames;
+    }
+
+    @Override
+    public String toString() {
+        return "Reader{" +
+                "name='" + name + '\'' +
+                ", cardNo=" + cardNo +
+                ", faculty='" + faculty + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", phone=" + phone +
+                ", bookNames=" + Arrays.toString(bookNames) +
+                '}';
+    }
 }
